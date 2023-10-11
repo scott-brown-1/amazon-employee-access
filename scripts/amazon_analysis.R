941ee5d441ace2e5a74ebd52ff66450da29862d6
Scott Brown <scottdbrown7@gmail.com>
2023-10-11 14:38:51 -0600

    Add penalized logistic regression

--- scripts/amazon_analysis.R
+++ scripts/amazon_analysis.R
@@ -13,13 +13,14 @@ prep_df <- function(df) {
   return(df)
 }
 
-setup_train_recipe <- function(df, other_threshold = 0.01, form=ACTION~.){
+setup_train_recipe <- function(df, other_threshold = 0.001, form=ACTION~.){
   prelim_ft_eng <- recipe(form, data=df) %>%
     step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
     step_other(all_nominal_predictors(), threshold = other_threshold) %>% # combines categorical values that occur <5% into an "other" value
-    step_dummy(all_nominal_predictors()) %>% # dummy variable encoding
-    step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) #target encoding
-  # also step_lencode_glm() and step_lencode_bayes()
+    step_dummy(all_nominal_predictors()) %>% # dummy variable encoding 
+    step_lencode_bayes(all_nominal_predictors(), outcome = vars(ACTION)) # Target encoding
+    #step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) #target encoding
+  # also step_lencode_glm()
   # NOTE: some of these step functions are not appropriate to use together
   
   # Set up preprocessing
@@ -27,16 +28,3 @@ setup_train_recipe <- function(df, other_threshold = 0.01, form=ACTION~.){
   
   return(prepped_recipe)
 }
-
-###########################
-####### Examine Data ######
-###########################
-
-## Load data
-train <- vroom::vroom('./data/train.csv')
-
-# # Set up preprocessing
-prepped_recipe <- setup_train_recipe(train)
-
-# Bake recipe
-dim(bake(prepped_recipe, new_data=train))
---
+++ scripts/penalized_logistic.R
@@ -0,0 +1,81 @@
+#########################
+### Imports and setup ###
+#########################
+
+library(tidyverse)
+library(tidymodels)
+
+source('./scripts/amazon_analysis.R')
+
+#########################
+####### Load Data #######
+#########################
+
+## Load data
+train <- prep_df(vroom::vroom('./data/train.csv'))
+test <- prep_df(vroom::vroom('./data/test.csv'))
+
+#########################
+## Feature Engineering ##
+#########################
+
+set.seed(42)
+
+## parallel tune grid
+doParallel::registerDoParallel(10)
+
+## Set up preprocessing
+prepped_recipe <- setup_train_recipe(train)
+
+## Bake recipe
+bake(prepped_recipe, new_data=train)
+bake(prepped_recipe, new_data=test)
+
+#########################
+## Fit Regression Model #
+#########################
+
+## Define model
+pen_logit_model <- logistic_reg(
+  mixture = tune(),
+  penalty = tune()) %>%
+  set_engine("glmnet")
+
+## Define workflow
+# Transform response to get different cutoff
+pen_logic_wf <- workflow(prepped_recipe) %>%
+  add_model(pen_logit_model)
+
+## Grid of values to tune over10
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 3)
+## Split data for CV
+folds <- vfold_cv(train, v = 3, repeats=1)
+
+## Run the CV
+cv_results <- pen_logic_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
+
+## Find optimal tuning params
+best_params <- cv_results %>%
+  select_best("roc_auc")
+
+## Fit workflow
+final_wf <- pen_logic_wf %>%
+  finalize_workflow(best_params) %>%
+  fit(data = train)
+
+## Predict new rentals
+y_pred <- predict(final_wf, new_data=test, type='prob')
+
+# Create output df in Kaggle format
+output <- data.frame(
+  Id=test$id,
+  Action=y_pred$.pred_1
+)
+
+#LS: penalty, then mixture
+vroom::vroom_write(output,'./outputs/penalized_logistic_predictions.csv',delim=',')
